ce2488d27529a628983c0779123d1e6dced04854
Christopher Wirkus <christopher@wirkus.me>
2026-01-05 10:21:49 +0100

    Update version display to 1.9 in VersionsPage and sidebar

--- src/pages/easybill/1.2/ShippingPage.tsx
+++ src/pages/easybill/1.2/ShippingPage.tsx
@@ -4000,7 +4000,7 @@ function ShippingPage() {
             </div>
             {/* Version number at bottom */}
             <div className="hidden lg:flex mt-auto text-xs text-white/60">
-              v1.8
+              v1.9
             </div>
           </div>
           {/* Left part - 300px width */}
